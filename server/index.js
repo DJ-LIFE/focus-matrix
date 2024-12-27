84022d1813d0161f7619199884eeaa006136ca49
gauravThakur-sgl <gaurav.thakur@shipglobal.in>
2024-12-27 18:00:52 +0530

    Added gitignore

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
-const port = 3000
+const port = 8081
+require('dotenv').config()
 
+const MONGO_URI = process.env.MONGO_URL
 
+const connectToDb = async () => {
+  try {
+    await mongoose.connect(MONGO_URI)
+    console.log('Connected to the database')
+  } catch (error) {
+    console.error('Error connecting to the database: ', error)
+  }
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -11,3 +22,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
